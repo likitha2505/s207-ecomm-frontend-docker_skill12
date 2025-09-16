5d90834898a67b3fe8be1adfd78146ea3027df09
likitha2505 <likkireddy2505@gmail.com>
2025-09-16 19:25:54 +0530

    Update Laptops.jsx

--- src/components/Laptops.jsx
+++ src/components/Laptops.jsx
@@ -4,7 +4,7 @@ import { getProducts } from "../services/productService";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://100.27.228.86/back1';
+const BASE_URL = 'http://3.88.90.113/back1';
 
 const Laptops = () => {
   const [products, setProducts] = useState([]);
